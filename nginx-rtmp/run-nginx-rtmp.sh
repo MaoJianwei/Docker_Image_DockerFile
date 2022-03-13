915c7ffb7803296d1f11827c704929dbc281c3ef
Jianwei Mao <maojianwei2012@126.com>
2022-03-12 22:21:14 -0800

    beta
    Signed-off-by: Jianwei Mao <maojianwei2012@126.com>

--- nginx-rtmp/run-nginx-rtmp.sh
+++ nginx-rtmp/run-nginx-rtmp.sh
@@ -2,7 +2,9 @@
 
 mkdir -p /nginx-conf/modules-enabled/
 mkdir -p /nginx-conf/sites-enabled/
+mkdir -p /nginx-live-hls/hls/
 mkdir -p /nginx-live-hls/record/
 mkdir -p /nginx-live-hls/mp4/
 
 /nginx-bin/nginx-rtmp -c /nginx-conf/nginx.conf -g "daemon off;"
+
